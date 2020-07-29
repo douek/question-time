05d8d42748f2543618a9082bed8ef496111214c5
Daniel Douek <dadouek@gmail.com>
2020-07-29 12:20:33 +0300

    Entry point of the app, and api get user
    Index.html template and handling the routing.
    API endpoint to get the current authenticate username

--- QuestionTime/urls.py
+++ QuestionTime/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django_registration.backends.one_step.views import RegistrationView
+from core.views import IndexTemplateView
 from users.forms import CustomUserForm
 
 urlpatterns = [
@@ -26,6 +27,8 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('users.api.urls')),
     path('api/rest-auth/', include('rest_auth.urls')),
     path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
+    re_path(r'^.*$', IndexTemplateView.as_view(),name='entry-point')
 ]
