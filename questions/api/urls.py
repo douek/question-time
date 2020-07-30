b19df0d54869fc959778556bba72e737a812c77a
Daniel Douek <dadouek@gmail.com>
2020-07-30 12:09:04 +0300

    Answer detail and like api endpoint

--- questions/api/urls.py
+++ questions/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from .views import QuestionViewSet, AnswerCreateAPIView, AnswerListAPIView
+from .views import QuestionViewSet
+from .views import AnswerCreateAPIView, AnswerListAPIView, AnswerRUDAPIView, AnswerLikeAPIView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -7,6 +8,8 @@ router.register(r'questions', QuestionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('questions/<slug:slug>/answer/', AnswerCreateAPIView.as_view(), name='create-answer'),
+    path('questions/<slug:slug>/answer/', AnswerCreateAPIView.as_view(), name='answer-create'),
     path('questions/<slug:slug>/answers/', AnswerListAPIView.as_view(), name='answer-list'),
+    path('answers/<int:pk>/', AnswerRUDAPIView.as_view(), name='answer-detail'),
+    path('answers/<int:pk>/like/', AnswerLikeAPIView.as_view(), name='answer-like'),
 ]
