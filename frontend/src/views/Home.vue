be452c00e93bb59bd6f3ea3a93abba7449e2f7dd
Daniel Douek <dadouek@gmail.com>
2020-08-07 17:42:07 +0300

    pagination - loading more questions

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="container">
+<div class="container mt-2">
   <div class="home">
     <div v-for="question in questions" :key="question.pk">
       <p class="mb-0"> Posted by <span class="author-name">{{ question.author }}</span></p>
@@ -8,6 +8,12 @@
       </router-link>
       <p>Answers: {{ question.answers_count }}</p>
       <hr>
+    </div>
+    <div class="my-4">
+      <p v-show="loadingQuestions">...Loading...</p>
+      <button v-show="next" @click="getQuestions" class="btn btn-sm btn-outline-secondary">
+        Load more
+      </button>
     </div>
   </div>
 </div>
@@ -19,14 +25,27 @@ import { apiService } from '../common/api_service'
 export default {
   name: "Home",
   data() {
-    return {questions:[]}
+    return {
+      questions:[],
+      next: null,
+      loadingQuestions: false
+      }
   },
   methods: {
     getQuestions() {
       let endpoint = 'api/questions/'
-
+      if (this.next){
+        endpoint = this.next
+      }
+      this.loadingQuestions = true
       apiService(endpoint).then(data => {
         this.questions.push(...data.results)
+        this.loadingQuestions = false
+        if(data.next) {
+          this.next = data.next
+        } else {
+          this.next = null
+        }
       })
     }
   },
